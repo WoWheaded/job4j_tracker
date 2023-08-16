cedf46a4b586a391cf9a6089172fac891a5d77c6
Axiom <d-343@yandex.ru>
2023-08-16 14:53:41 +0300

    2. Идентичные тексты [#212655]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,21 @@
+package ru.job4j.collection;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class UniqueText {
+    public boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>(Arrays.asList(origin));
+        for (String textString : text) {
+            if (!check.contains(textString)) {
+                rsl = false;
+                break;
+            }
+
+        }
+        return rsl;
+    }
+}
