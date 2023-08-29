a631cb677ad83a87b3203b749e6bd88d282df865
Axiom <d-343@yandex.ru>
2023-08-29 14:00:04 +0300

    3. Отсортировать департаменты [#34352]

---
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -0,0 +1,13 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class DepDescComp implements Comparator<String> {
+    @Override
+    public int compare(String o1, String o2) {
+        String[] obj1 = o1.split("/");
+        String[] obj2 = o2.split("/");
+        int rsl = obj2[0].compareTo(obj1[0]);
+        return rsl == 0 ? o1.compareTo(o2) : rsl;
+    }
+}
