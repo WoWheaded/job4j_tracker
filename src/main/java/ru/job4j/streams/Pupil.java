830973a7940d4ccfe71b29572d7174d470436ae2
Axiom <d-343@yandex.ru>
2023-08-30 20:05:13 +0300

    3. Аттестация [#49777]

---
+++ src/main/java/ru/job4j/streams/Pupil.java
@@ -0,0 +1,6 @@
+package ru.job4j.streams;
+
+import java.util.List;
+
+public record Pupil(String name, List<Subject> subjects) {
+}
---
+++ src/main/java/ru/job4j/streams/Subject.java
@@ -0,0 +1,4 @@
+package ru.job4j.streams;
+
+public record Subject(String name, int score) {
+}
