2e3a9c45feb805d9b3472025cbd684bd05ad6df6
Axiom <d-343@yandex.ru>
2023-08-30 13:07:13 +0300

    17. Тестовое задание из модуля коллекции Lite переделать на Stream API. [#24260]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -7,6 +7,7 @@ import java.util.Map;
 
 /**
  * Класс, представляющий банковский сервис для управления пользователями и их счетами
+ *
  * @author DMITRY ARININ
  * @version 1.0
  */
@@ -20,6 +21,7 @@ public class BankService {
      * Метод добавления нового пользователя в коллекцию
      * Если такой пользователь уже есть в коллекции - добавление не происходит
      * Если такой пользователь в коллекции новый - он добавляется
+     *
      * @param user который добавляется в коллекцию
      */
     public void addUser(User user) {
@@ -28,6 +30,7 @@ public class BankService {
 
     /**
      * Удаляет пользователя из коллекции на основе его паспортных данных
+     *
      * @param passport Паспортные данные пользователя
      * @return {@code true} если пользователь был успешно удален, {@code false} если пользователь не найден
      */
@@ -37,6 +40,7 @@ public class BankService {
 
     /**
      * Добавляет банковский счет к пользователю по его паспортным данным
+     *
      * @param passport Паспортные данные пользователя
      * @param account  Добавляемый банковский счет
      */
@@ -49,42 +53,39 @@ public class BankService {
 
     /**
      * Поиск пользователя по его паспортным данным
+     *
      * @param passport Паспортные данные пользователя
      * @return Объект пользователя или {@code null}, если пользователь не найден
      */
     public User findByPassport(String passport) {
-        User findUser = null;
-        for (User user : users.keySet()) {
-            if (user.getPassport().equals(passport)) {
-                findUser = user;
-                break;
-            }
-        }
-        return findUser;
+        return users.keySet().stream()
+                .filter(e -> e.getPassport().equals(passport))
+                .findFirst()
+                .orElse(null);
     }
 
     /**
      * Поиск банковского счета пользователя по реквизитам
+     *
      * @param passport  Паспортные данные пользователя
      * @param requisite Реквизиты банковского счета
      * @return Банковский счет или {@code null}, если счет не найден
      */
     public Account findByRequisite(String passport, String requisite) {
-        Account account = null;
         User user = findByPassport(passport);
         if (user != null) {
-            for (Account acc : getAccounts(user)) {
-                if (requisite.equals(acc.getRequisite())) {
-                    account = acc;
-                    break;
-                }
-            }
+            return getAccounts(user)
+                    .stream()
+                    .filter(acc -> requisite.equals(acc.getRequisite()))
+                    .findFirst()
+                    .orElse(null);
         }
-        return account;
+        return null;
     }
 
     /**
      * Переводит указанную сумму денег между банковскими счетами
+     *
      * @param srcPassport   Паспортные данные отправителя
      * @param srcRequisite  Реквизиты банковского счета отправителя
      * @param destPassport  Паспортные данные получателя
@@ -107,6 +108,7 @@ public class BankService {
 
     /**
      * Получает список банковских счетов, принадлежащих указанному пользователю
+     *
      * @param user Пользователь, для которого нужно получить список счетов
      * @return Список банковских счетов пользователя
      */
